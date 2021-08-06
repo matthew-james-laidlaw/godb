1aca6f7b07138e418a8d9deed3a4049bccb47c5b
Matt Laidlaw <mlaidlaw01@gmail.com>
2021-08-05 21:30:15 -0700

    EventLoop method drives interaction with StorageEngine

---
+++ internal/pkg/interaction/interaction.go
@@ -0,0 +1,44 @@
+package interaction
+
+import (
+	"GoDB/internal/pkg/parse"
+	"GoDB/internal/pkg/storage"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+func EventLoop(store storage.ObjectStore, reader *bufio.Reader, writer *bufio.Writer) {
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF || line == "q\n" || line[0] == 4 {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		stmt, err := parse.Parse(line)
+		if err != nil {
+			_, err = writer.WriteString(err.Error())
+			if err != nil {
+
+			}
+			err = writer.Flush()
+			if err != nil {
+
+			}
+			continue
+		}
+
+		res := stmt.Execute(store)
+		_, err = writer.WriteString(res)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = writer.Flush()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
